f424eb25106f2b1277269a36472be5ba9baf93d0
shunsuke101 <sasakishunsuke1010@gmail.com>
2026-09-02 12:27:02 +0900

    add plugin oid and kanagawa and delete colerscheme cambell

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,14 +2,33 @@ vim.g.mapleader=" "
 vim.g.maplocalleader="\\"
 
 vim.opt.expandtab=true
-vim.opt.shiftwidth=2
-vim.opt.tabstop=2
+
+vim.opt.shiftwidth=8
+
+vim.opt.tabstop=8
+vim.opt.softtabstop=8
+
+vim.opt.smarttab=true
+vim.opt.smartindent=true
+vim.opt.autoindent=true
+
+vim.opt.cursorline=true
+
+--Store undo between session
+vim.opt.undofile=true
+
+vim.opt.showmode=false
+
 vim.opt.ignorecase=true
 vim.opt.smartcase=true
+
+vim.opt.signcolumn="yes"
+
+vim.opt.splitright=true
+vim.opt.splitbelow=true
+
 vim.opt.clipboard="unnamedplus"
 vim.opt.termguicolors=true
 
 require("config.lazy")
-
-vim.cmd.colorscheme("campbell")
-
+        
